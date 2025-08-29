78de525540da7078dcdd0cda24e040df3c729151
Jesus B <itzdier862@gmail.com>
2025-08-29 17:41:16 -0600

    Se agregaron nuevas visualizaciones y metodos para nuestra app ToDo's

--- app/views.py
+++ app/views.py
@@ -3,5 +3,14 @@ from flask import Blueprint, render_template
 main = Blueprint('main', __name__)
 
 @main.route('/')
-def home():
-    return render_template('home.html')
+# def home():
+#    return render_template('home.html')
+
+# def dashboard():
+#    return render_template('dashboard.html')
+
+# def dashboard_gestionable():
+#    return render_template('dashboard_gestionable.html')
+
+def dashboard_pro():
+    return render_template('dashboard_pro.html')
